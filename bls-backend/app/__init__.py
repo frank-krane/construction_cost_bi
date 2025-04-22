0ad34e6d02c0e24f2a386cfd0e363b0406d8e6b5
Frank Lee <frank@krane.tech>
2025-04-21 21:03:09 -0700

    Loose COR constraint

--- bls-backend/app/__init__.py
+++ bls-backend/app/__init__.py
@@ -18,7 +18,8 @@ class Config:
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    #CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+    CORS(app)
     app.config.from_object(Config())
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
