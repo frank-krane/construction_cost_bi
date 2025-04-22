c61b7dd3c970c38e6126a7eec0bfe909f2011662
Frank Lee <franklee@Franks-MacBook-Air.local>
2025-04-21 19:35:23 -0700

    Disable Eslint errors

--- bls-frontend/src/app/utils/material-utils.ts
+++ bls-frontend/src/app/utils/material-utils.ts
@@ -76,6 +76,7 @@ export const groupMaterialData = (
 /**
  * Debounce utility function
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function debounce<T extends (...args: any[]) => any>(
     fn: T,
     delayMs: number
--- bls-frontend/src/components/material-chart/material-chart.tsx
+++ bls-frontend/src/components/material-chart/material-chart.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/prefer-as-const */
+
 "use client";
 
 import React, { useEffect, useMemo, useState } from "react";
