8ce7be7d1c5f609aad8387c5b49f716f1946254c
Frank Lee <frank@krane.tech>
2025-04-21 20:56:35 -0700

    Listening to all interfaces. Temporary workaround to make it work

--- bls-backend/run.py
+++ bls-backend/run.py
@@ -31,4 +31,4 @@ def handle_database_creation_and_seeding():
 
 if __name__ == '__main__':
     handle_database_creation_and_seeding()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
